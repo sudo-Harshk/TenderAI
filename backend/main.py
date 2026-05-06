63432d1fc358966edb7be629ec812a59c41bec6d
sudo-Harshk <harshk1744@gmail.com>
2026-05-07 00:47:22 +0530

    modified:   backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="TenderAI API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
